26c6631435fa132ec06313876c356430b727be9f
agostinoluca <agostino.luca89@gmail.com>
2024-01-19 20:47:36 +0100

    add style to page for first test

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,18 @@ export default {
 <!-- /header -->
 
 <main>
-
+  <button>This is a button</button>
 </main>
 <!-- /main -->
 
 <footer>
-
+  <ul class="test">
+    <li><a href="#">this is a link</a></li>
+    <li><a href="#">this is a link</a></li>
+    <li><a href="#">this is a link</a></li>
+    <li><a href="#">this is a link</a></li>
+    <li><a href="#">this is a link</a></li>
+  </ul>
 </footer>
 <!-- /footer -->
 </template>
