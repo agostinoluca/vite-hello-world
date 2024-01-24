bcde32a8e7f0937f0607180a0f6d106950bb2ee7
agostinoluca <agostino.luca89@gmail.com>
2024-01-24 20:50:48 +0100

    fix button delete components folder add style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <script>
 export default {
   name: 'App',
-}
+  data(){
+    return {
+      type: 'button',
+      link: 'this is a link',
+      name: '',
+      surname: '',
+      count: 0
+    }
+  },
+  methods: {
+    testFunction(){
+      console.log('Sto testando methods');
+      this.testFunction2();
+      this.testFunction3();
+    },
+    testFunction2(){
+      console.log('Funziono?');
+    },
+    testFunction3(){
+      this.count++;
+    }
+  }
+};
 </script>
 
 <template>
@@ -11,17 +33,28 @@ export default {
 <!-- /header -->
 
 <main>
-  <button>This is a button</button>
+  <div class="inputTest">
+    <label for="name">Name</label>
+    <input type="text" v-model="name">
+
+    <label for="surname">Surname</label>
+    <input type="text" v-model="surname">
+
+  </div>
+  <button @click="testFunction">
+    <p>Hello {{ name + ' ' + surname }}</p>
+    <p>You click this {{ type }} {{ count }} times</p>
+  </button>
 </main>
 <!-- /main -->
 
 <footer>
   <ul class="test">
-    <li><a href="#">this is a link</a></li>
-    <li><a href="#">this is a link</a></li>
-    <li><a href="#">this is a link</a></li>
-    <li><a href="#">this is a link</a></li>
-    <li><a href="#">this is a link</a></li>
+    <li><a href="#">{{ link }}</a></li>
+    <li><a href="#">{{ link }}</a></li>
+    <li><a href="#">{{ link }}</a></li>
+    <li><a href="#">{{ link }}</a></li>
+    <li><a href="#">{{ link }}</a></li>
   </ul>
 </footer>
 <!-- /footer -->
